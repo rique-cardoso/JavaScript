d76092ce9ff01d0c3b9de073130397d464fd22da
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-09-11 16:33:27 -0300

    continuação do exercicio

--- Módulo L/ex005/script.js
+++ Módulo L/ex005/script.js
@@ -1,21 +1,24 @@
 function converterBases() {
     const baseConvertida = document.querySelector("#convertido").value
     const baseConversora = document.querySelector("#conversor").value
-    const baseConvertida_valor = Number(document.querySelector("#valorConvertido").value)
+    const baseConvertida_valor = [...document.querySelector("#valorConvertido").value]
     let resultado = document.querySelector("#resultado")
     if(baseConvertida != baseConversora){
         switch (baseConvertida) {
             case "bin":
-                switch (baseConversora) {
-                    case "oct":
-                        
-                        break;
-                    case "dec":
-                        
-                        break;
-                
-                    default:
-                        break;
+                if(baseConvertida_valor.every(elem => elem <= 1 && elem >= 0)){
+                    switch (baseConversora) {
+                        case "oct":
+                            
+                            break;
+                        case "dec":
+                            
+                            break;
+                        default:
+                            break;
+                    }
+                }else{
+                    alert("A base binária só pode conter apenas o dígitos 0 e 1.")
                 }
                 break;
             case "oct":
