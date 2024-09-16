89a76be146e4cc1f86b10a44e3c2d8c73b2f50e8
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-09-16 13:37:58 -0300

    ex006 terminado

--- Módulo L/ex006/script.js
+++ Módulo L/ex006/script.js
@@ -1,33 +1,47 @@
 const pessoas = []
 const registrar = document.querySelector("#registrar")
 const encerrar = document.querySelector("#encerrar")
-
+const resultado = document.querySelector("#resultado")
 function limparCampos(nome, salario, numeroFilhos) {
     nome.value = ""
     salario.value = ""
     numeroFilhos.value = ""
 }
-
+function formatarNumero(num, casasDecimais = 2) {
+    return Number(num).toFixed(casasDecimais).replace('.', ',')
+}
 registrar.addEventListener('click', e => {
     const nome = document.querySelector("#nome")
     const salario = document.querySelector("#salario")
     const numeroFilhos = document.querySelector("#nmrFilhos")
-    const pessoa = {
-        nome: nome.value,
-        salario: salario.value,
-        numeroFilhos: numeroFilhos.value
+    if(nome.value == "" || salario.value == "" || numeroFilhos.value == ""){
+        alert("Por favor, preencha todos os campos para registrar uma pessoa.")
+    }else{
+        const pessoa = {
+            nome: nome.value,
+            salario: salario.value,
+            numeroFilhos: numeroFilhos.value
+        }
+        pessoas.push(pessoa)
+        limparCampos(nome, salario, numeroFilhos)
     }
-    pessoas.push(pessoa)
-    limparCampos(nome, salario, numeroFilhos)
 })
 
 encerrar.addEventListener('click', e => {
-    e.preventDefault()
-    const somaSalario = pessoas.reduce((prev, now) => Number(prev) + Number(now.salario), 0)
-    const somaFilhos = pessoas.reduce((prev, now) => Number(prev) + Number(now.numeroFilhos), 0)
-    const maiorSalario = Math.max(...pessoas.map(elem => Number(elem.salario)))
-    const menor150 = pessoas.filter(e => Number(e.salario) < 150)
-    for (const elemento of menor150) {
-        console.log(elemento.salario)
+    if(pessoas.length > 0){
+        const somaSalario = pessoas.reduce((prev, now) => Number(prev) + Number(now.salario), 0)
+        const mediaSalario = somaSalario / pessoas.length
+        const somaFilhos = pessoas.reduce((prev, now) => Number(prev) + Number(now.numeroFilhos), 0)
+        const mediaFilhos = somaFilhos / pessoas.length
+        const maiorSalario = Math.max(...pessoas.map(elem => Number(elem.salario)))
+        const menor150 = pessoas.filter(e => Number(e.salario) < 150)
+        const percentual_menor150 = (100 * menor150.length) / pessoas.length
+        resultado.innerHTML = `
+            Média salarial da população: R$ ${formatarNumero(mediaSalario)}<br>
+            Média de filhos da população: ${formatarNumero(mediaFilhos, 1)}<br>
+            Maior salário da população: R$ ${formatarNumero(maiorSalario)}<br>
+            Percentual de pessoas em extrema pobreza: ${formatarNumero(percentual_menor150)}%`
+    }else{
+        alert("Não há ninguém cadastrado ainda, por favor, realize um cadastro para consultar as informações")
     }
 })
