fc250a663a312d0b4e3b125bf8a8dc7e592bc680
Henrique Cardoso <henrique.prates.br@gmail.com>
2024-11-05 15:46:35 -0300

    Prática de Classes - parte 2

--- Módulo L/pratica-de-classes/script.js
+++ Módulo L/pratica-de-classes/script.js
@@ -10,16 +10,33 @@ let alturaPalco = palco.offsetHeight //pega a altura total do objeto palco
 let bolas = []
 let numBolas = 0
 
+class Bola{
+    constructor(){
+        this.tamanho = Math.floor(Math.random() * 15) + 10
+        this.cor_red = Math.floor(Math.random() * 255)
+        this.cor_green = Math.floor(Math.random() * 255)
+        this.cor_blue = Math.floor(Math.random() * 255)
+        this.posicao_x = Math.floor(Math.random() * (larguraPalco - this.tamanho))
+        this.posicao_y = Math.floor(Math.random() * alturaPalco)
+    }
+}
+
 //função para atualização dos valores de largura e altura da tela quando esta for redimensionada
 window.addEventListener('resize', e => {
     larguraPalco = palco.offsetWidth
     alturaPalco = palco.offsetWidth
 })
 
-btn_add.addEventListener('click', e => {
 
+btn_add.addEventListener('click', e => {
+    const qtde = txt_qtde.value
+    for(let i = 0; i < qtde; i++){
+        //Instanciar novas bolinhas
+    }
 })
 
 btn_remover.addEventListener('click', e => {
-    
+    bolas.map(e => {
+        //Remover bolas
+    })
 })
