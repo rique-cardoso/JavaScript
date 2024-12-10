55b28b4a422e5289cedc59e847df8b03cb5839b6
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-10 11:53:39 -0300

    Módulos em JS - p1

---
+++ Módulo M/modulos/p1/cursos.js
@@ -0,0 +1,4 @@
+const cursos = ['JavaScript', 'HTML', 'CSS', 'Arduíno', 'Raspberry', 'C++', 'Python', 'Java', 'C#']
+const carros = ['Polo', 'T-Cross', 'Golf', 'Onix', 'Cruze', 'Argo', 'HRV']
+
+export {cursos}
---
+++ Módulo M/modulos/p1/script.js
@@ -0,0 +1,4 @@
+import { cursos } from "./cursos.js" //precisa da extensão do arquivo '.js' senão dá erro
+
+console.log('Rodando...')
+console.log(cursos)
