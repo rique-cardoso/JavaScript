810ae12307f5a60976c96a0e82caa381f0f4fb8b
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-17 10:48:58 -0300

    Módulos parte 4

--- Módulo M/modulos/p4/script.js
+++ Módulo M/modulos/p4/script.js
@@ -1,5 +1,5 @@
 // import Cursos from "./cursos.js"
 import c from "./cursos.js"
 console.log(c.addCurso("Django"))
-// console.log(c.apagarCurso())
+console.log(c.apagarCurso())
 console.log(c.getTodosCursos())
