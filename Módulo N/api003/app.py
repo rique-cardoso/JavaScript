49aaf99304bda4ef621911e5f52bf482fd676efc
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-24 12:39:21 -0300

    API 003
    Estudos de API - criando uma API Python que consome uma API pública e usando um arquivo js para consumir essa api python e renderizar no html

--- Módulo N/api003/app.py
+++ Módulo N/api003/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 import requests
 import random
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get-character', methods=['GET'])
 def get_character():
--- Módulo N/api003/script.js
+++ Módulo N/api003/script.js
@@ -2,5 +2,19 @@ const btnSorteio = document.querySelector('#sorteio')
 const divImg = document.querySelector('#img')
 
 btnSorteio.addEventListener('click', () => {
-    
+    divImg.innerHTML = '<p>Carregando...</p>' // Indicador de carregamento
+
+    // Faz a requisição para o backend Python
+    fetch('http://127.0.0.1:5000/get-character').then(response => response.json()).then(data => {
+        if(data.image){
+            const img = document.createElement('img')
+            img.src = data.image
+            divImg.innerHTML = '' // Limpa o indicador
+            divImg.appendChild(img)
+        } else{
+            divImg.innerHTML = '<p>Erro ao carregar imagem. Tente novamente!</p>'
+        }
+    }).catch(() => {
+        divImg.innerHTML = '<p>Erro ao comunicar com o servidor. Tente novamente!</p>'
+    })
 })
