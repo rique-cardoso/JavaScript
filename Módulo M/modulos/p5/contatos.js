b0e21be8b13a00911e0bf1ba791a32dff2378b52
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2024-12-28 15:18:40 -0300

    Melhorando o exercício de módulos - p6

--- Módulo M/modulos/p5/contatos.js
+++ Módulo M/modulos/p5/contatos.js
@@ -13,18 +13,21 @@ let contato = {
             email: novoContato.email
         }
         contatos.push(pessoa)
-        const div = document.createElement('div')
-        div.setAttribute('class', 'contato')
-        const nome = document.createElement('p')
-        nome.innerHTML = novoContato.nome
-        const telefone = document.createElement('p')
-        telefone.innerHTML = novoContato.telefone
-        const email = document.createElement('p')
-        email.innerHTML = novoContato.email
-        div.appendChild(nome)
-        div.appendChild(telefone)
-        div.appendChild(email)
-        destinoDom.appendChild(div)
+        destinoDom.innerHTML = ""
+        contatos.map(elem => {
+            const div = document.createElement('div')
+            div.setAttribute('class', 'contato')
+            const nome = document.createElement('p')
+            nome.innerHTML = elem.nome
+            const telefone = document.createElement('p')
+            telefone.innerHTML = elem.telefone
+            const email = document.createElement('p')
+            email.innerHTML = elem.email
+            div.appendChild(nome)
+            div.appendChild(telefone)
+            div.appendChild(email)
+            destinoDom.appendChild(div)
+        })
     }
 }
 export default contato
