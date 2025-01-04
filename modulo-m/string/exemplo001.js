9bbc44699732c9a67a174342de86e497a560c9e4
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-04 10:26:22 -0300

    String - exemplo001.js
    formas de declaração de string

---
+++ modulo-m/string/exemplo001.js
@@ -0,0 +1,11 @@
+// Uma string em JavaScript é uma sequência de caracteres. Elas podem ser criadas com aspas simples ('), aspas duplas (") ou template literals (`).
+const string1 = "Olá, mundo!" // Aspas duplas
+const string2 = 'JavaScript é muito divertido!' // Aspas simples
+const string3 = `Templates String permitem
+qubra de linha` // Template Strings
+const variavel = 15
+const string4 = `Templates Strings permitem, também, concatenar uma variável de forma fácil, olhe -> ${variavel}`
+console.log(string1)
+console.log(string2)
+console.log(string3)
+console.log(string4)
