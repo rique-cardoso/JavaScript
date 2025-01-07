a4138c507ded595e9b2a9fe4d1ca27b00a5b6d78
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-01-07 15:49:12 -0300

    Regex - exemplo001.js
    Declarando uma regex

---
+++ modulo-m/regex/exemplo001.js
@@ -0,0 +1,6 @@
+const regex = /\d+/ // um ou mais dígitos
+const regex1 = new RegExp("\\d+") // um ou mais dígitos
+console.log(regex) // /\d+/
+console.log(typeof(regex)) // object
+console.log(regex1) // /\d+/
+console.log(typeof(regex1)) // object
