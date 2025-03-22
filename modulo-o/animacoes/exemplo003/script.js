e706b6746c179105e2eff66b0b5891f982a6397e
Henrique Prates (DELL - Aninha) <henrique.prates.br@gmail.com>
2025-03-22 20:27:13 -0300

    exemplo003 animações

--- modulo-o/animacoes/exemplo003/script.js
+++ modulo-o/animacoes/exemplo003/script.js
@@ -4,12 +4,28 @@ const btn_esquerda = document.querySelector('#btn_esquerda')
 const btn_direita = document.querySelector('#btn_direita')
 
 const init = () => {
-    carro.style = "position:relative;left:0px"
+    carro.style = "position:relative;left:0px;width:100px"
+    tamMax = window.innerWidth - parseInt(carro.style.width)
 }
+
 let anima = null
+let tamMax = null
 const mover = (dir) => {
-    carro.style.left = parseInt(carro.style.left) + (10*dir) + "px"
-    anima = setTimeout(mover, 20, dir)
+    if(dir > 0){
+        if(parseInt(carro.style.left) <= tamMax){
+            carro.style.left = parseInt(carro.style.left) + (10*dir) + "px"
+            anima = setTimeout(mover, 20, dir)
+        }else{
+            clearTimeout(anima)
+        }
+    }else if(dir < 0){
+        if(parseInt(carro.style.left) >= 0){
+            carro.style.left = parseInt(carro.style.left) + (10*dir) + "px"
+            anima = setTimeout(mover, 20, dir)
+        }else{
+            clearTimeout(anima)
+        }
+    }
 }
 btn_parar.addEventListener('click', e => {
     clearTimeout(anima)
@@ -23,3 +39,6 @@ btn_direita.addEventListener('click', e => {
     mover(1)
 })
 window.addEventListener("load", init())
+window.addEventListener("resize", () => {
+    window.innerWidth - parseInt(carro.style.width) 
+})
